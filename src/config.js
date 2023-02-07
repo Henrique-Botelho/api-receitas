728c759f951a299a67d225d9b9c7a9c95af77a02
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-02-07 11:01:16 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASS = process.env.DB_PASS || "";
 const DB_PORT = process.env.DB_PORT || 3306;
-const KEY_ACCESS = "4WAPlNmInAy2ZTkIAMy9"
+const KEY_ACCESS = process.env.KEY_ACCESS;
 
 module.exports = {
     PORT,
